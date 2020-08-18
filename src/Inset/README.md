faae240028ce2982931e9d0d4a1201761e0f763b
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-08-18 22:52:15 +0300

    + Inset

---
+++ src/Inset/README.md
@@ -0,0 +1,28 @@
+# `Inset`
+
+```css
+.Inset\(0$\) {
+  top: 0$;
+  left: 0$;
+  bottom: 0$;
+  right: 0$;
+}
+
+.InsetX\(0$\) {
+  left: 0$;
+  right: 0$;
+}
+.InsetY\(0$\) {
+  top: 0$;
+  bottom: 0$;
+}
+
+```
+
+## Usage
+
+```html
+<div class="Inset(0)"></div>
+<div class="InsetX(0)"></div>
+<div class="InsetY(0)"></div>
+```
---
+++ src/Inset/index.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    type: "helper",
+    name: "Inset",
+    matcher: "Inset",
+    styles: {
+      top: "$0",
+      left: "$0",
+      bottom: "$0",
+      right: "$0"
+    }
+  },
+  {
+    type: "helper",
+    name: "InsetX",
+    matcher: "InsetX",
+    styles: {
+      left: "$0",
+      right: "$0"
+    }
+  },
+  {
+    type: "helper",
+    name: "InsetY",
+    matcher: "InsetY",
+    styles: {
+      top: "$0",
+      bottom: "$0"
+    }
+  },
+];
